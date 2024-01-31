3b3727564619979dbdf15d1af73682d40ffb67ef
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-01-31 13:55:19 -0500

    Create trending.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

---
+++ widgets/views/trending.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+use humhub\widgets\PanelMenu;
+use humhub\modules\topic\widgets\TopicLabel;
+
+/* @var $this \humhub\modules\ui\view\components\View */
+/* @var $topics \humhub\modules\topic\models\Topic[] */
+
+?>
+<div class="panel panel-default panel-topic" id="panel-topic">
+    <?= PanelMenu::widget(['id' => 'panel-topic']) ?>
+    <div class="panel-heading">
+        <?= Yii::t('TopicModule.base', '<strong>Trending</strong> Topics') ?>
+    </div>
+    <div class="panel-body">
+        <?php foreach ($topics as $topic): ?>
+            <?= TopicLabel::forTopic($topic) ?>
+        <?php endforeach; ?>
+    </div>
+</div>
