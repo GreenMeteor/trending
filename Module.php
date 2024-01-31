b2637c52934357bade1cf159909d9db7442d33ed
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-01-31 14:07:23 -0500

    Update Module.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- Module.php
+++ Module.php
@@ -4,7 +4,28 @@ namespace gm\humhub\modules\trending;
 
 use humhub\components\Module as BaseModule;
 
+/**
+ * Class Module
+ *
+ * This class represents the trending module in HumHub.
+ *
+ * @package gm\humhub\modules\trending
+ */
 class Module extends BaseModule
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'Trending';
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription(): string
+    {
+        return 'Provides functionality for displaying trending topics.';
+    }
 }
